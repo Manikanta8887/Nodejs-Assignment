16fe2760b5665cc0df4ed700f9f413cc52042a2d
Manikanta <swamimanikanta66@gmail.com>
2025-01-21 11:53:57 +0530

    Update usermodel.js

--- MVC/Models/usermodel.js
+++ MVC/Models/usermodel.js
@@ -12,8 +12,8 @@ exports.users = () => {
     })
 }
 
-exports.register = (req,res) => { 
-    var {cid,cname,cemail,cage} = req.body
+exports.register = (data) => { 
+    var {cid,cname,cemail,cage} = data
     var query = "Insert into practicenode (cid,cname,cemail,cage) values(?,?,?,?)"
     return new Promise((resolve, reject) => {
         connection.query(query,[cid,cname,cemail,cage],(err, data) => {
